db548dee035e1c37da67594128daa5ed82f59279
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-08 10:58:47 +0100

    Add AdventureCard abstract class

---
+++ src/main/java/AdventureCard.java
@@ -0,0 +1,12 @@
+public abstract class AdventureCard {
+    abstract void choosePlanet();
+    abstract void grabGoods();
+    abstract void placeGoods();
+    abstract void spendBatteries();
+    abstract void submitPower();
+    abstract int rollDice();
+    abstract int grabCredits();
+    abstract void loseResident();
+    abstract void loseBatteries();
+    abstract void activate();
+}
