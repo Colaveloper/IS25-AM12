8bf057a95c6d6460ab03848de06ee42737fbb5e1
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-12 21:43:26 +0100

    Add Choice enum class for card choices

---
+++ src/main/java/adventureCards/utils/Choice.java
@@ -0,0 +1,13 @@
+package adventureCards.utils;
+
+public enum Choice {
+    GRAB_GOODS,
+    PLACE_GOODS,
+    SPEND_BATTERIES,
+    SUBMIT_POWER,
+    GRAB_CREDITS,
+    LOSE_RESIDENT,
+    LOSE_BATTERIES,
+    LOSE_FLIGHTDAYS,
+    CHOOSE_PLANET
+}
