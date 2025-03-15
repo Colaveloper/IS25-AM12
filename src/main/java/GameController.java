06cfb0858f6fb99cdce1a5368bc906465dd44ba3
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-15 23:28:23 +0100

    Comment out code to avoid unecessary errors

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,94 +1,94 @@
-import adventureCards.utils.Choice;
-
-import adventureCards.AdventureCard;
-import java.util.List;
-
-public class GameController {
-    private GameModel model;
-    private GameView view;
-    private int currentPlayer;
-    private Choice choice;
-    private Boolean submit;
-
-    //example interaction between controller with view and model
-//    public void startGame(){
-//        model.startGame();
-//        view.showWelcomeMessage(); //show the initial screen
-//        play();
-//    }
+//import adventureCards.utils.Choice;
 //
-//    public void play(){
-//        while(!model.isGameOver(){
-//            view.displayBoard(model.getBoard()); //shipboard and flightboard
-//            handlePlayerAction();
-//            model.endTurn();
+//import adventureCards.AdventureCard;
+//import java.util.List;
+//
+//public class GameController {
+//    private GameModel model;
+//    private GameView view;
+//    private int currentPlayer;
+//    private Choice choice;
+//    private Boolean submit;
+//
+//    //example interaction between controller with view and model
+////    public void startGame(){
+////        model.startGame();
+////        view.showWelcomeMessage(); //show the initial screen
+////        play();
+////    }
+////
+////    public void play(){
+////        while(!model.isGameOver(){
+////            view.displayBoard(model.getBoard()); //shipboard and flightboard
+////            handlePlayerAction();
+////            model.endTurn();
+////        }
+////        view.displayWinner(model.getWinner());
+////    }
+//
+//    // Cards management
+//    public void drawCard() {
+//        model.drawCard();
+//        currentPlayer = model.getCurrenPlayer();
+//        view.displayCard(card);
+//
+//
+//        while (choice = model.nextStep() != Choice.END_CARD) {
+//            switch(choice){
+//                case GRAB_GOODS -> model.grabGoods(view.getInput(currentPlayer));
+//                case GRAB_CREDITS -> {      //useless if getting credits is automatic
+//                    if (view.getInput(currentPlayer)){
+//                        model.grabCredits();
+//                    }
+//                }
+//                case CHOOSE_PLANET -> {
+//                   //TODO: update model and view
+//                }
+//                case LOSE_RESIDENT -> {
+//                    model.loseResident();
+//                }
+//                case SUBMIT_POWER -> {
+//                    model.submitPower();
+//                }
+//                case ACTIVATE_SHIELD -> {
+//                    for (Integer i : model.getProjectileDirections()) {
+//                        if (model.checkShieldDirection(i)) {
+//                            model.removeComponent(i);
+//                        }
+//                        else if (view.getInput(currentPlayer)) { //activate shield?
+//                            model.removeComponent(i);
+//                        }
+//                    }
+//                }
+//
+//                case ACTIVATE_CANNON -> {
+//                    while(!submit) {
+//                        model.askShipboard();
+//                        model.activateCannon(view.getInput(currentPlayer));//
+//                    }
+//                }
+//
+//                case ACTIVATE_ENGINE -> {
+//                    while(!submit) {
+//                        model.activateEngine(view.getInput(currentPlayer));
+//                    }
+//                }
+//
+//                case PLACE_GOODS -> {
+//                    model.placeGoods();
+//                }
+//                case CONSUME_BATTERIES -> {    //not necessary if put in lose goods
+//                    //model.loseBatteries(n);
+//                }
+//                case ASK_NEXT_PLAYER -> {
+//                    model.resetStep();
+//                    flightBoard.nextPlayer();
+//                    currentPlayer = flightBoard.getCurrentPlayer();
+//                }
+//
+//                default -> view.showError("Invalid choice");
+//
+//            }
 //        }
-//        view.displayWinner(model.getWinner());
 //    }
-
-    // Cards management
-    public void drawCard() {
-        model.drawCard();
-        currentPlayer = model.getCurrenPlayer();
-        view.displayCard(card);
-
-
-        while (choice = model.nextStep() != Choice.END_CARD) {
-            switch(choice){
-                case GRAB_GOODS -> model.grabGoods(view.getInput(currentPlayer));
-                case GRAB_CREDITS -> {      //useless if getting credits is automatic
-                    if (view.getInput(currentPlayer)){
-                        model.grabCredits();
-                    }
-                }
-                case CHOOSE_PLANET -> {
-                   //TODO: update model and view
-                }
-                case LOSE_RESIDENT -> {
-                    model.loseResident();
-                }
-                case SUBMIT_POWER -> {
-                    model.submitPower();
-                }
-                case ACTIVATE_SHIELD -> {
-                    for (Integer i : model.getProjectileDirections()) {
-                        if (model.checkShieldDirection(i)) {
-                            model.removeComponent(i);
-                        }
-                        else if (view.getInput(currentPlayer)) { //activate shield?
-                            model.removeComponent(i);
-                        }
-                    }
-                }
-
-                case ACTIVATE_CANNON -> {
-                    while(!submit) {
-                        model.askShipboard();
-                        model.activateCannon(view.getInput(currentPlayer));//
-                    }
-                }
-
-                case ACTIVATE_ENGINE -> {
-                    while(!submit) {
-                        model.activateEngine(view.getInput(currentPlayer));
-                    }
-                }
-
-                case PLACE_GOODS -> {
-                    model.placeGoods();
-                }
-                case CONSUME_BATTERIES -> {    //not necessary if put in lose goods
-                    //model.loseBatteries(n);
-                }
-                case ASK_NEXT_PLAYER -> {
-                    model.resetStep();
-                    flightBoard.nextPlayer();
-                    currentPlayer = flightBoard.getCurrentPlayer();
-                }
-
-                default -> view.showError("Invalid choice");
-
-            }
-        }
-    }
-}
+//}
--- src/main/java/adventureCards/OpenSpaceCard.java
+++ src/main/java/adventureCards/OpenSpaceCard.java
@@ -1,63 +1,63 @@
-package adventureCards;
-
-import adventureCards.utils.Choice;
-import adventureCards.utils.Goods;
-import adventureCards.utils.Projectile;
-
-import java.util.Arrays;
-import java.util.List;
-
-
-public class OpenSpaceCard extends AdventureCard{
-
-    public OpenSpaceCard() {
-        super(Arrays.asList(
-                Choice.ACTIVATE_ENGINE,
-                Choice.SUBMIT_POWER,
-                Choice.ASK_NEXT_PLAYER,
-                Choice.END_CARD
-        ));
-    }
-
-    @Override
-    public List<Boolean> getPlanets() { //do nothing
-        return null;
-    }
-
-    @Override
-    public int getFirePower() {         //do nothing
-        return 0;
-    }
-
-    @Override
-    public int getCredits() {           //do nothing
-        return 0;
-    }
-
-    @Override
-    public List<Goods> getGoods() {     //do nothing
-        return null;
-    }
-
-    @Override
-    public List<Integer> getProjectileDirections() {//do nothing
-        return null;
-    }
-
-    @Override
-    public List<Projectile> getProjectilesType() {  //do nothing
-        return null;
-    }
-
-    @Override
-    public int getSacrifice() {         //do nothing
-        return 0;
-    }
-
-
-    @Override
-    public void landOnPlanet(int i) {   //do nothing
-
-    }
-
-}
+//package adventureCards;
+//
+//import adventureCards.utils.Choice;
+//import adventureCards.utils.Goods;
+//import adventureCards.utils.Projectile;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//
+//public class OpenSpaceCard extends AdventureCard{
+//
+//    public OpenSpaceCard() {
+//        super(Arrays.asList(
+//                Choice.ACTIVATE_ENGINE,
+//                Choice.SUBMIT_POWER,
+//                Choice.ASK_NEXT_PLAYER,
+//                Choice.END_CARD
+//        ));
+//    }
+//
+//    @Override
+//    public List<Boolean> getPlanets() { //do nothing
+//        return null;
+//    }
+//
+//    @Override
+//    public int getFirePower() {         //do nothing
+//        return 0;
+//    }
+//
+//    @Override
+//    public int getCredits() {           //do nothing
+//        return 0;
+//    }
+//
+//    @Override
+//    public List<Goods> getGoods() {     //do nothing
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Integer> getProjectileDirections() {//do nothing
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Projectile> getProjectilesType() {  //do nothing
+//        return null;
+//    }
+//
+//    @Override
+//    public int getSacrifice() {         //do nothing
+//        return 0;
+//    }
+//
+//
+//    @Override
+//    public void landOnPlanet(int i) {   //do nothing
+//
+//    }
+//
+//}
