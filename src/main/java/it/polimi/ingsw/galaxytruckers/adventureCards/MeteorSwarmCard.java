35e0efb5402f66b102affda0d35fba4552ef4407
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-16 17:15:08 +0100

    Add MeteorSwarmCard

--- src/main/java/it/polimi/ingsw/galaxytruckers/adventureCards/MeteorSwarmCard.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/adventureCards/MeteorSwarmCard.java
@@ -1,68 +1,82 @@
-//package adventureCards;
-//
-//import adventureCards.utils.CardState;
-//import adventureCards.utils.Goods;
-//import adventureCards.utils.Projectile;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class MeteorSwarmCard extends AdventureCard{
-//    private final List<Integer> projectileDirections;
-//    private final List<Projectile> projectileTypes;
-//
-//
-//    public MeteorSwarmCard(List<Integer> projectileDirections, List<Projectile> projectileTypes) {
-//        super(Arrays.asList(
-//                CardState.ACTIVATE_CANNON,
-//                CardState.ASK_NEXT_PLAYER,
-//                CardState.END_CARD
-//        ));
-//        this.projectileDirections = projectileDirections;
-//        this.projectileTypes = projectileTypes;
-//    }
-//    @Override
-//    public List<Boolean> getPlanets() {     //do nothing
-//        return null;
-//    }
-//
-//
-//    @Override
-//    public int getFirePower() {     //do nothing
-//        return 0;
-//    }   //do nothing
-//
-//    @Override
-//    public int getCredits() {       //do nothing
-//        return 0;
-//    }   //do nothing
-//
-//    @Override
-//    public List<Goods> getGoods() {         //do nothing
-//        return null;
-//    }
-//
-//    @Override
-//    public List<Integer> getProjectileDirections() {
-//        return projectileDirections;
-//    }
-//
-//    @Override
-//    public List<Projectile> getProjectilesType() {
-//        return projectileTypes;
-//    }
-//
-//    @Override
-//    public int getSacrifice() {     //do nothing
-//        return 0;
-//    }   //do nothing
-//
-//
-//    @Override
-//    public void landOnPlanet(int i) {       //do nothing
-//
-//    }
-//
-//
-//
-//}
+package it.polimi.ingsw.galaxytruckers.adventureCards;
+
+import it.polimi.ingsw.galaxytruckers.GameModel;
+import it.polimi.ingsw.galaxytruckers.adventureCards.utils.CardState;
+import it.polimi.ingsw.galaxytruckers.adventureCards.utils.Projectile;
+import it.polimi.ingsw.galaxytruckers.enumTypes.GoodsType;
+
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeteorSwarmCard extends AdventureCard{
+    //attributes
+    private List<Integer> projectileDirections;
+    private List<Projectile> projectileType;
+
+    public MeteorSwarmCard(List<Integer> projectileDirections, List<Projectile> projectileType){
+        //attributes init
+        this.projectileDirections = new ArrayList<>(projectileDirections);
+        this.projectileType = new ArrayList<>(projectileType);
+
+        //cardState init
+        cardStates = new ArrayList<>();
+        for (int i = 0; i < projectileType.size(); i++) {
+            if(projectileType.get(i) == Projectile.SMALL_METEOR){
+                cardStates.add(CardState.ACTIVATE_SHIELD);
+            } else if (projectileType.get(i) == Projectile.BIG_METEOR) {
+                cardStates.add(CardState.ACTIVATE_CANNON);
+            } else {
+                //TODO: make this launch some kind of exception or proper error message
+                System.out.println("ERROR: PROJECTILE OF INCORRECT TYPE IN CONSTRUCTOR");
+            }
+        }
+        cardStates.add(CardState.END_CARD);
+    }
+
+    //USED METHODS
+    @Override
+    public CardState nextStep(GameModel model) {
+        step++;
+        return cardStates.get(step);
+    }
+    @Override
+    public List<Integer> getProjectileDirections() {
+        return projectileDirections;
+    }
+    @Override
+    public List<Projectile> getProjectilesType() {
+        return projectileType;
+    }
+
+    //UNUSED METHODS
+    @Override
+    public List<Boolean> getPlanets() {
+        return null;
+    }
+    @Override
+    public int getFirePower() {
+        return 0;
+    }
+    @Override
+    public int getCredits() {
+        return 0;
+    }
+    @Override
+    public List<GoodsType> getGoods() {
+        return null;
+    }
+    @Override
+    public int getSacrifice() {
+        return 0;
+    }
+
+    @Override
+    public int getFlightDaysLost() {
+        return 0;
+    }
+    @Override
+    public void landOnPlanet(int i) {
+
+    }
+}
