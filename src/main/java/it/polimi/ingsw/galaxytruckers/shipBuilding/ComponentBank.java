0c0f94227eb0a3c3874978d4afa84ec439aef7f4
capbros <tommaso.capone@gmail.com>
2025-03-16 17:44:24 +0100

    Fix ComponentBank internal implementation
    Add empty collection management and switch Deque with List (we only need to remove from the top)

--- src/main/java/it/polimi/ingsw/galaxytruckers/shipBuilding/ComponentBank.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/shipBuilding/ComponentBank.java
@@ -1,18 +1,22 @@
 package it.polimi.ingsw.galaxytruckers.shipBuilding;
 
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
 
 public class ComponentBank {
-    private Deque<Component> coveredComponents;
+    private List<Component> coveredComponents;
     private Map<Integer, Component> uncoveredComponents;
 
     public Component getComponent(int id) {
+        if (!uncoveredComponents.containsKey(id)) {
+            throw new IllegalArgumentException("No component with id " + id + " exists");
+        }
         return uncoveredComponents.remove(id);
     }
 
     public Component getRanComponent() {
-        return coveredComponents.poll();
+        return coveredComponents.removeLast();
     }
 
     public void addUncovered(Component component) {
