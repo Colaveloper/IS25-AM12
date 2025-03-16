ec765ecc89f3975f6b865feec1a25ffc408878ad
Francesco Cola <63930332+Colaveloper@users.noreply.github.com>
2025-03-16 18:03:38 +0100

    Add Ship-Building Methods to GameModel

--- src/main/java/it/polimi/ingsw/galaxytruckers/GameModel.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/GameModel.java
@@ -11,6 +11,7 @@ import it.polimi.ingsw.galaxytruckers.Deck;
 import it.polimi.ingsw.galaxytruckers.TempDeck;
 import it.polimi.ingsw.galaxytruckers.adventureCards.AdventureCard;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -30,33 +31,50 @@ public class GameModel {
         deck = gameFactory.createDeck();
     }
 
-    //FLIGHTBOARD-RELATED METHODS
-
+    // UTILITY METHODS
+    /**
+     * @return translation from a player ID to his ShipBoard
+     */
     private ShipBoard getShipFromPlayer(int playerId) {
+        if (!idsToShip.containsKey(playerId)) {
+            throw new IllegalArgumentException("No player with such id");
+        }
         return idsToShip.get(playerId);
     }
-
+    /**
+     * @return translation from a player's ShipBoard to his ID
+     */
     private Integer getPlayerFromShip(ShipBoard shipBoard) {
+        if (!idsToShip.inverse().containsKey(shipBoard)) {
+            throw new IllegalArgumentException("No such shipboard");
+        }
         return idsToShip.inverse().get(shipBoard);
     }
 
-    // return value to be interpreted as "building phase is finished for everybody"
+    // FLIGHTBOARD-RELATED METHODS
+    /** @return true iff the building phase is finished for everybody
+     * @throws IllegalArgumentException if the starting position is not legal
+     */
     public boolean placeShipOnFlightBoard(int playerId, int startingPosition) {
         return flightBoard.placeShipOnFlightBoard(getShipFromPlayer(playerId), startingPosition);
     }
-
+    /**
+     * @return  a list of player IDs (players ahead in the flight have lower index)
+     */
     public List<Integer> getOrderedPlayers() {
         return flightBoard.getOrderedShips()
                 .stream()
                 .map(this::getPlayerFromShip)
                 .collect(Collectors.toList());
     }
-
+    /** Moves the player's ship on the FlightBoard
+     */
     public void displaceShip(int playerId, int displacement) {
         flightBoard.displaceShip(getShipFromPlayer(playerId), displacement);
-    }
-
-    // returns playerId mapped to finalScore
+    } // TODO: hide from controller
+    /**
+     * @return  for each player ID, the score he obtained at the end of the game
+     */
     public Map<Integer, Integer> getFinalScores() {
         return flightBoard.getFinalScores().entrySet()
                 .stream()
@@ -64,18 +82,75 @@ public class GameModel {
                         entry -> getPlayerFromShip(entry.getKey()), Map.Entry::getValue
                 ));
     }
-
-    public void playerGivesUp(int playerId) {
+    /** Makes the player with the specified ID give up
+     * */
+    public void chooseToGiveUp(int playerId) {
         flightBoard.giveUp(getShipFromPlayer(playerId));
     }
-
-    // (returns empty set in test flight)
+    /**
+     * @return players lapped by the leader, or the empty set if in test flight
+     */
     public Set<Integer> getLappedPlayers () {
         return flightBoard.getLappedShips()
                 .stream()
                 .map(this::getPlayerFromShip)
                 .collect(Collectors.toSet());
     }
+
+    // SHIP-BUILDING METHODS
+    /**
+     * Makes a covered component the last component
+     */
+    public void requestRanComponent(int playerId) {
+        getShipFromPlayer(playerId).requestRandComponent();
+    } // TODO: handle concurrency!
+    /**
+     * Makes the requested uncovered component the last component
+     */
+    public void requestComponent(int playerId, int componentId) {
+        getShipFromPlayer(playerId).requestComponent(componentId);
+    } // TODO: handle concurrency!
+    /**
+     * Makes the last component available to other players
+     */
+    public void rejectComponent(int playerId) {
+        getShipFromPlayer(playerId).rejectComponent();
+    }
+    /**
+     * Rotates anti-clockwise the last component
+     * @throws IllegalStateException if no component can be turned
+     */
+    public void rotateComponent (int playerId) {
+        getShipFromPlayer(playerId).rotateComponent();
+    }
+    /**
+     * Stashes up to two last components for each player
+     */
+    public void stashComponent (int playerId) {
+        getShipFromPlayer(playerId).stashComponent();
+    } // TODO: make available for second flight only!
+    /**
+     * Un-stashes the indexed component, making it the last component
+     * @throws IllegalArgumentException if there is not such component in the stash
+     */
+    public void takeStashedComponent (int playerId, int componentId) {
+        getShipFromPlayer(playerId).getStashedComponent(componentId);
+    } // TODO: make available for second flight only!
+    /**
+     * Places the last component in the specified position of the ship of the specified player
+     */
+    public void placeComponent(int playerId, Point position) {
+        getShipFromPlayer(playerId).placeComponent(position);
+    } // TODO: handle illegal positions!
+
+    void removeComponent(int playerId, Point position) {
+        getShipFromPlayer(playerId).removeComponent(position);
+    } // TODO: hide from controller, consider whether to remove
+
+
+
+
+
 
     //TEMPORARY CODE, FOR TESTING ONLY ---------------------------------------------
     public GameModel(){
