58a12d0e07ac234e46dbc3f5db8ab3cba4d69d94
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-16 23:22:07 +0100

    Add all card state options

--- src/main/java/it/polimi/ingsw/galaxytruckers/Main.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/Main.java
@@ -20,11 +20,12 @@ public class Main {
         //simulating drawing the AbandonedShip card
 
         //List<CardState> activeCardState = model.getCardStates();
-        System.out.println("The " + model.getCardName() + " card has been drawn\nResident sacrifice: " +
-                model.getCardSacrifice() + "\nCredit gains: " + model.getCardCredits()  +
-                "\nFlight days cost: " + model.getCardFlightDaysLost());
+//        System.out.println("The " + model.getCardName() + " card has been drawn\nResident sacrifice: " +
+//                model.getCardSacrifice() + "\nCredit gains: " + model.getCardCredits()  +
+//                "\nFlight days cost: " + model.getCardFlightDaysLost());
 
 
+        System.out.println("The " + model.getCardName() + " card has been drawn!");
         while(cardState != CardState.END_CARD){
             switch (cardState){
                 case ASK_NEXT_PLAYER:
@@ -47,6 +48,83 @@ public class Main {
                     break;
                 case LOSE_RESIDENT:
                     model.loseResidents(model.getCardSacrifice());//makes the current player lose residents
+                    break;
+                case SABOTAGE:
+                    model.sabotage();
+                    break;
+                case ACTIVATE_CANNON:
+                    //user input
+                    System.out.println("Do you want to activate a CANNON to increase power? (y/n):");
+                    input = userScanner.nextLine();
+                    if (input.equals("y")) {
+                        model.activateCannon(3, 7);
+                    } else if (input.equals("n")) {
+                        //just continues onto next step
+                        //model.getCardStates();
+                    } else {
+                        System.out.println("Invalid input. Going ahead to next step");
+                        //model.getCardState();
+                    }
+                    break;
+                case ACTIVATE_SHIELD:
+                    //user input
+                    System.out.println("Do you want to activate a SHIELD to increase power? (y/n):");
+                    input = userScanner.nextLine();
+                    if (input.equals("y")) {
+                        System.out.println("Press ENTER to roll the dice");
+                        input = userScanner.nextLine();
+                        model.activateShield(3, 7);
+                    } else if (input.equals("n")) {
+                        System.out.println("Press ENTER to roll the dice");
+                        input = userScanner.nextLine();
+                        model.fireCannonAtPlayer();
+                        model.rollDice();
+                    } else {
+                        System.out.println("Invalid input. Going ahead to next step");
+                    }
+                    break;
+                case GRAB_GOODS:
+                    model.grabGoods(model.getCardGoods());
+                    break;
+                case LOSE_GOODS:
+                    //TODO: potentially remove this from controller
+                    //it may be completely automatic
+                    break;
+                case START_CARD:
+                    System.out.println("PLAYING THE " + model.getCardName() + " CARD");
+                    break;
+                case GET_BLASTED:
+                    System.out.println("PLAYING THE " + model.getCardName() + " CARD");
+                    break;
+                case SUBMIT_POWER:
+                    System.out.println("PLAYING THE " + model.getCardName() + " CARD");
+                    break;
+                case CHOOSE_PLANET:
+                    int numPlanetInput;
+                    //TODO: fix this to account for number of planets available
+                    System.out.println("Choose planet (0,1,2)");
+                    numPlanetInput = userScanner.nextInt();
+                    if(numPlanetInput != 0 || numPlanetInput != 1 || numPlanetInput != 2){
+                        System.out.println("Invalid planet to land on. Going to next player");
+                        model.passCardToNextPlayer();
+                    }
+                    else{
+                        model.landOnPlanet(numPlanetInput);
+                    }
+                    break;
+                case ACTIVATE_ENGINE:
+                    //user input
+                    System.out.println("Do you want to activate an ENGINE to increase power? (y/n):");
+                    input = userScanner.nextLine();
+                    if (input.equals("y")) {
+                        model.activateEngine(3, 7);
+                    } else if (input.equals("n")) {
+                        //just continues onto next step
+                        //model.getCardStates();
+                    } else {
+                        System.out.println("Invalid input. Going ahead to next step");
+                        //model.getCardState();
+                    }
                     break;
                 default:
                     System.out.println("Error in processing card choice");
