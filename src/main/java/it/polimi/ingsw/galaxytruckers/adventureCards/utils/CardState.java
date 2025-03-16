70e62e51acfd0c1660824840b07d5273ee2848c5
Lorenzo Carafa <lorenzo.carafa@mail.polimi.it>
2025-03-16 18:26:28 +0100

    in CardState enum deleted unused states, added two more

--- src/main/java/it/polimi/ingsw/galaxytruckers/adventureCards/utils/CardState.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/adventureCards/utils/CardState.java
@@ -1,19 +1,16 @@
 package it.polimi.ingsw.galaxytruckers.adventureCards.utils;
 
 public enum CardState {
-    GRAB_GOODS,
-    PLACE_GOODS,
-    //SPEND_BATTERIES,  //use more specific methods
-    ACTIVATE_SHIELD,
-    ACTIVATE_CANNON,
-    ACTIVATE_ENGINE,
-    SUBMIT_POWER,
-    LOSE_GOODS,
-    //GRAB_CREDITS,
-    LOSE_RESIDENT,
-    CONSUME_BATTERIES,
-    //LOSE_FLIGHTDAYS,
-    CHOOSE_PLANET,
-    ASK_NEXT_PLAYER,
-    END_CARD
+    GRAB_GOODS,         //player places goods on his shipboard or none
+    ACTIVATE_SHIELD,    //player rolls for small meteors and small cannons and chooses to activate
+    ACTIVATE_CANNON,    //player rolls for big meteors and chooses to activate
+    GET_BLASTED,        //player rolls for big cannon and loses pieces
+    ACTIVATE_ENGINE,    //player chooses to activate engine
+    SUBMIT_POWER,       //player chooses if he wants to stop activating stuff
+    LOSE_GOODS,         //player chooses what goods to lose
+    LOSE_RESIDENT,      //player chooses what residents to lose
+    CHOOSE_PLANET,      //player chooses planet
+    ASK_NEXT_PLAYER,    //player chooses if he wants to pass
+    START_CARD,         //doesn't require input but single-action would end before doing automatic parts(stardust, epidemic)
+    END_CARD            //flag to end card
 }
