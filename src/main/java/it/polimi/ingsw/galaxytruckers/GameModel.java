accadc1d7635293ed8206ee67992c3c5f7a25b5b
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-16 23:21:41 +0100

    Add additional card methods

--- src/main/java/it/polimi/ingsw/galaxytruckers/GameModel.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/GameModel.java
@@ -6,10 +6,11 @@ import it.polimi.ingsw.galaxytruckers.Deck;
 import it.polimi.ingsw.galaxytruckers.TempDeck;
 import it.polimi.ingsw.galaxytruckers.adventureCards.AdventureCard;
 import it.polimi.ingsw.galaxytruckers.adventureCards.utils.CardState;
+import it.polimi.ingsw.galaxytruckers.enumTypes.GoodsType;
 
 import java.util.List;
 
-public class GameModel {
+public class GameModel{
     private Deck deck;
     private AdventureCard activeCard;
     private int maxCardPlays; //TEMPORARY, defines the maximum times a card can be played
@@ -108,10 +109,50 @@ public class GameModel {
         System.out.println("Player with least number of residents has lost " + flightDaysLost + " days");
     }
 
-//    public int throwDice(Boolean activatable) {
-//        i = rand;
-//        if(activatable) {
-//            shipboard.setDice(i);
-//        }
-//    }
+    public void sabotage(){
+        //TODO: select player with least amount of residents
+        //roll two dice for column and two dice for row
+        //lose that component
+        System.out.println("Player with least amount of residents has been sabotaged!");
+    }
+
+    public void grabGoods(List<GoodsType> goodsList){
+        //TODO: give goods to current player
+        System.out.println("Current player has been given the goods");
+    }
+
+    public List<GoodsType> getCardGoods(){
+        return activeCard.getGoods();
+    }
+
+    //TODO: possibly condense these three methods into one method ------------------------------------
+    public void activateEngine(int x, int y){
+        //TODO: increase the current players engine power by spending batteries
+        System.out.println("Increasing engine power at (x=" + x +",y="+ y +") for the current player");
+    }
+
+    public void activateCannon(int x, int y){
+        //TODO: increase the current players engine power by spending batteries
+        System.out.println("Increasing cannon power at (x=" + x +",y="+ y +") for the current player");
+    }
+
+    public void activateShield(int x, int y){
+        //TODO: increase the current players engine power by spending batteries
+        System.out.println("Increasing shield power at (x=" + x +",y="+ y +") for the current player");
+    }
+    //TODO: ----------------------------------------------------------------------------------------------
+
+    public void landOnPlanet(int i){
+        //TODO: current player lands on planet
+        activeCard.landOnPlanet(i);
+    }
+
+    public void fireCannonAtPlayer(){
+        //TODO: roll dice and fire
+    }
+
+    public int rollDice(){
+        Dice dice = Dice.create();
+        return dice.roll();
+    }
 }
