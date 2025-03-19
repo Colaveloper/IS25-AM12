7f4d595ff5c7478275c1cd5b706e27e39eded68b
Francesco Canossi <canossifrancesco4@protonmail.com>
2025-03-19 21:17:49 +0100

    Add loadComponents method to read JSON file and generate components

--- src/main/java/it/polimi/ingsw/galaxytruckers/shipBuilding/ComponentBank.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/shipBuilding/ComponentBank.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.galaxytruckers.shipBuilding;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class ComponentBank {
@@ -34,5 +39,40 @@ public class ComponentBank {
 
     public void addUncovered(Component component) {
         uncoveredComponents.put(component.hashCode(), component);
+    }
+
+    public static List<Component> loadComponents(File jsonFile) throws IOException{
+        //reading from json file and adding to coveredComponents
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonFile);
+
+        List<Component> components = new ArrayList<>();
+
+        for (JsonNode node : rootNode){
+            String type = node.get("type").asText();
+            Component component;
+            List<Connector> connectors = parseConnectors(node.get("connectors"));
+
+            switch(type){
+                case "shield":
+                    component = new Shield(connectors);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown component type: " + type);
+            }
+
+            components.add(component);
+        }
+        return components;
+    }
+
+    private static List<Connector> parseConnectors(JsonNode connectorNode){
+        List<Connector> connectors = new ArrayList<>();
+        if(connectorNode != null && connectorNode.isArray()){
+            for (JsonNode conn : connectorNode){
+                connectors.add(Connector.valueOf(conn.asText().toUpperCase()));
+            }
+        }
+        return connectors;
     }
 }
