d2aa94862ff084899092627c590d4d04a1e831f5
Francesco Cola <63930332+Colaveloper@users.noreply.github.com>
2025-03-21 23:59:56 +0100

    Add Physical Interface on FlightBoard and ShipBoard

--- src/main/java/it/polimi/ingsw/galaxytruckers/SecondFlightBoard.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/SecondFlightBoard.java
@@ -2,17 +2,22 @@ package it.polimi.ingsw.galaxytruckers;
 
 import it.polimi.ingsw.galaxytruckers.enumTypes.Level;
 import it.polimi.ingsw.galaxytruckers.shipBuilding.ShipBoard;
+import javafx.scene.image.Image;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SecondFlightBoard extends FlightBoard{
+    private static Image image;
+
     public SecondFlightBoard(Set<ShipBoard> allShips) {
         super(allShips);
         this.loopLength = Level.SECOND.getLoopLength();
         this.startingPositionsLeft = new ArrayList<>(Level.SECOND.getStartingPositions().subList(0, allShips.size()));
+        image = new Image("file:src/main/resources/texture/cardboard/second-flight-board.png");
     }
 
     @Override
@@ -21,7 +26,7 @@ public class SecondFlightBoard extends FlightBoard{
         if (!startingPositionsLeft.contains(startingPosition)) {
             throw new IllegalArgumentException("Position not available for start");
         } else {
-            startingPositionsLeft.remove(startingPositionsLeft.indexOf(startingPosition));
+            startingPositionsLeft.remove((Integer) startingPosition);
             shipToPlace.put(shipBoard, startingPosition);
             // to be interpreted as "building phase is finished for everybody"
             return startingPositionsLeft.size() + allShips.size() == 4;
@@ -29,20 +34,44 @@ public class SecondFlightBoard extends FlightBoard{
     }
 
     @Override
-    public Set<ShipBoard> getLappedShips() {
-        return shipToPlace.entrySet().stream()
+    public void removeShips (Set<ShipBoard> shipsToRemove) {
+        shipToPlace.entrySet().removeIf(entry -> shipsToRemove.contains(entry.getKey()));
+        shipsToRemove.forEach(ship ->finalScores.put(ship, (ship.getGoodsValue() + 1) / 2 + ship.getCredits() - ship.getLosses()));
+    }
+
+    @Override
+    public Set<ShipBoard> getAndRemoveLappedShips() {
+        System.out.println(shipToPlace.entrySet().stream()
+                .filter(entry -> shipToPlace.get(getOrderedShips().getFirst()) - entry.getValue() > loopLength).toString());
+
+        Set<ShipBoard> lappedShips = shipToPlace.entrySet().stream()
                 .filter(entry -> shipToPlace.get(getOrderedShips().getFirst()) - entry.getValue() > loopLength)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toSet());
+
+        removeShips(lappedShips);
+
+        return lappedShips;
     }
 
     @Override
     public void giveUp(ShipBoard shipBoard) {
-        removeShip(shipBoard);
-        finalScores.put(shipBoard, (shipBoard.getGoodsValue() + 1) / 2 + shipBoard.getCredits() - shipBoard.getLosses());
+        removeShips(Set.of(shipBoard));
         if (allShips.size() - finalScores.size() == 1) {
             // TODO: one-player is left!
             //  ignore the Combat Zone and Sabotage adventures
         }
+    }
+
+    @Override
+    public Image getImage() {
+        return image;
+        // TODO: composite ships on top
+    }
+
+    @Override
+    public String getDescription() {
+        return "Second-Flight: "+super.getDescription();
+        // TODO: describe positions
     }
 }
