eedcd0bd023feb9a083f1fab5663a666352c2007
froopy090 <canossifrancesco4@protonmail.com>
2025-05-20 14:44:54 +0200

    Add test, work in progress

---
+++ src/test/java/it/polimi/ingsw/galaxytruckers/model/state/RemoveGoodsStateTest.java
@@ -0,0 +1,125 @@
+package it.polimi.ingsw.galaxytruckers.model.state;
+
+import it.polimi.ingsw.galaxytruckers.model.Deck;
+import it.polimi.ingsw.galaxytruckers.model.Game;
+import it.polimi.ingsw.galaxytruckers.model.SecondDeck;
+import it.polimi.ingsw.galaxytruckers.model.adventureCards.AdventureCard;
+import it.polimi.ingsw.galaxytruckers.model.enumTypes.FourColors;
+import it.polimi.ingsw.galaxytruckers.model.enumTypes.Level;
+import it.polimi.ingsw.galaxytruckers.model.shipBuilding.SecondShipBoard;
+import it.polimi.ingsw.galaxytruckers.model.shipBuilding.ShipBoard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveGoodsStateTest {
+    RemoveGoodsState testState;
+    ShipBoard ship1;
+    Game game;
+    Deck deck;
+    AdventureCard adventureCard;
+
+    @BeforeEach
+    void setup(){
+        ship1 = new SecondShipBoard(FourColors.BLUE);
+    }
+
+    void setupGame() throws IOException {
+        game = new Game(Level.SECOND){
+            @Override
+            public Deck getDeck(){
+                return deck;
+            }
+        };
+        adventureCard = new AdventureCard(game, Level.SECOND, 1) {
+            @Override
+            public GameState nextStep() {
+                return new AdventureState();
+            }
+        };
+        deck = new SecondDeck(game){
+            @Override
+            public AdventureCard getCurrentCard(){
+                return adventureCard;
+            }
+        };
+    }
+
+    @Test
+    void setGameChangesStateWhenNoGoodsToLose() throws IOException {
+        setupGame();
+        testState = new RemoveGoodsState(0, ship1);
+        testState.setGame(game);
+        assertEquals(AdventureState.class, game.getCurrentState().getClass());
+    }
+
+    @Test
+    void setGameDoesNotChangeStateWhenShipHasNoGoodsValue() throws IOException {
+        ship1 = new SecondShipBoard(FourColors.BLUE){
+            @Override
+            public int getGoodsValue(){
+                return 0;
+            }
+            @Override
+            public int getNumBatteries(){
+                return 1;
+            }
+        };
+        setupGame();
+        testState = new RemoveGoodsState(1, ship1);
+        testState.setGame(game);
+        assertNull(game.getCurrentState());
+    }
+
+    @Test
+    void setGameDoesNotChangeStateWhenShipHasPositiveGoodsValue() throws IOException {
+        ship1 = new SecondShipBoard(FourColors.BLUE){
+            @Override
+            public int getGoodsValue(){
+                return 1;
+            }
+            @Override
+            public int getNumBatteries(){
+                return 1;
+            }
+        };
+        setupGame();
+        testState = new RemoveGoodsState(1, ship1);
+        testState.setGame(game);
+        assertNull(game.getCurrentState());
+    }
+
+    @Test
+    void setGameChangesStateWhenShipHasBatteries() throws IOException {
+        ship1 = new SecondShipBoard(FourColors.BLUE){
+            @Override
+            public int getGoodsValue(){
+                return 0;
+            }
+            @Override
+            public int getNumBatteries(){
+                return 0;
+            }
+        };
+        setupGame();
+        testState = new RemoveGoodsState(1, ship1);
+        testState.setGame(game);
+        assertEquals(AdventureState.class, game.getCurrentState().getClass());
+    }
+
+    @Test
+    void loseGoodThrowsExceptionWhenOutOfTurn(){
+        ShipBoard ship2 = new SecondShipBoard(FourColors.RED);
+        testState = new RemoveGoodsState(2, ship2);
+        assertThrows(IllegalStateException.class, () -> testState.loseGood(ship1, new Point(7,7)));
+    }
+
+    @Test
+    void loseGoodRemovesMostValuableGood(){
+        // TODO: finish test
+    }
+}
