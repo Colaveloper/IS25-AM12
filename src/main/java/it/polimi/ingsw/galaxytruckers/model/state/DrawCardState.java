613e66db13c0392e6cf184760f0fbc79772423e5
capbros <tommaso.capone@gmail.com>
2025-05-24 20:59:50 +0200

    Restore lost fix
    restore lost change from "Fix FlightBoard related behavior"

--- src/main/java/it/polimi/ingsw/galaxytruckers/model/state/DrawCardState.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/model/state/DrawCardState.java
@@ -1,9 +1,8 @@
 package it.polimi.ingsw.galaxytruckers.model.state;
 
 import it.polimi.ingsw.galaxytruckers.model.Game;
+import it.polimi.ingsw.galaxytruckers.model.enumTypes.Level;
 import it.polimi.ingsw.galaxytruckers.model.shipBuilding.ShipBoard;
-
-import java.util.Set;
 
 public class DrawCardState extends AdventureState {
     ShipBoard shipBoard;
@@ -12,9 +11,11 @@ public class DrawCardState extends AdventureState {
     public void setGame(Game game) {
         super.setGame(game);
         this.shipBoard = game.getFlightBoard().getOrderedShips().getFirst();
-        game.forceShipsToGiveUp();
-        game.endGameIfAllShipsHaveGivenUp();
-        game.getFlightBoard().removeShips(game.getGivenUpShips());
+        if (game.getLevel() == Level.SECOND) {
+            game.forceShipsToGiveUp();
+            game.endGameIfAllShipsHaveGivenUp();
+            game.getFlightBoard().removeShips(game.getGivenUpShips());
+        }
     }
 
     @Override
