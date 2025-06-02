61b8422567ae8ccd5afd18e0e4852c12c8d9bf7c
Francesco Cola <63930332+Colaveloper@users.noreply.github.com>
2025-06-03 00:34:49 +0200

    Fix: minor cli fix

--- src/main/java/it/polimi/ingsw/galaxytruckers/view/cliElements/CliShipBoard.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/view/cliElements/CliShipBoard.java
@@ -87,9 +87,9 @@ public class CliShipBoard extends CliElement {
             result.addAll(rowDescription);
         }
 
-        StringBuilder xIndexes = new StringBuilder("   ");
+        StringBuilder xIndexes = new StringBuilder(" ");
         for (int x = minX; x <= maxX; x++) {
-            xIndexes.append("  ").append(x).append("   ");
+            xIndexes.append("  ").append(x).append("  ");
         }
         result.add(xIndexes.toString());
 
