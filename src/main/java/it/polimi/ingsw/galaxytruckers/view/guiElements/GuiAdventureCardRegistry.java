64f2d254d524dc98db85d5f83ab76f7834411059
capbros <tommaso.capone@gmail.com>
2025-06-18 16:41:38 +0200

    Fix GUI image registries

--- src/main/java/it/polimi/ingsw/galaxytruckers/view/guiElements/GuiAdventureCardRegistry.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/view/guiElements/GuiAdventureCardRegistry.java
@@ -10,9 +10,8 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GuiAdventureCardRegistry {
+public class GuiAdventureCardRegistry extends GuiImageRegistry{
     private static GuiAdventureCardRegistry instance;
-    private final Map<Integer, Image> idToImage;
 
     public static GuiAdventureCardRegistry getInstance() {
         if (instance == null) {
@@ -22,27 +21,6 @@ public class GuiAdventureCardRegistry {
     }
 
     private GuiAdventureCardRegistry() {
-        idToImage = new HashMap<>();
-        loadImages();
-    }
-
-    private void loadImages() {
-        AdventureCardRegistry adventureCardRegistry = AdventureCardRegistry.getInstance();
-        Map<Integer, Path> idToPath = adventureCardRegistry.getIdToImagePath();
-        for (int i = 1; i < adventureCardRegistry.getSize(); i++) {
-            try (InputStream is = Files.newInputStream(idToPath.get(i))) {
-                Image image = new Image(is);
-                if (image.isError()) {
-                    System.err.println("Image failed to load: " + image.getException());
-                }
-                idToImage.put(i, image);
-            } catch (IOException e) {
-                System.err.println("Path not found: " + idToPath.get(i) + " for image " + i);
-            }
-        }
-    }
-
-    public Image getImage(int id) {
-        return idToImage.get(id);
+        loadImages(AdventureCardRegistry.getInstance().getIdToImagePath());
     }
 }
--- src/main/java/it/polimi/ingsw/galaxytruckers/view/guiElements/GuiComponentRegistry.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/view/guiElements/GuiComponentRegistry.java
@@ -1,18 +1,9 @@
 package it.polimi.ingsw.galaxytruckers.view.guiElements;
 
 import it.polimi.ingsw.galaxytruckers.view.controller.ComponentRegistry;
-import javafx.scene.image.Image;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-
-public class GuiComponentRegistry {
+public class GuiComponentRegistry extends GuiImageRegistry{
     private static GuiComponentRegistry instance;
-    private final Map<Integer, Image> idToImage;
 
     public static GuiComponentRegistry getInstance() {
         if (instance == null) {
@@ -22,28 +13,6 @@ public class GuiComponentRegistry {
     }
 
     private GuiComponentRegistry() {
-        idToImage = new HashMap<>();
-        loadImages();
-    }
-
-    private void loadImages() {
-        ComponentRegistry componentRegistry = ComponentRegistry.getInstance();
-        Map<Integer, Path> idToPath = componentRegistry.getIdToImagePath();
-
-        for (int i = 1; i < componentRegistry.getSize(); i++) {
-            try (InputStream is = Files.newInputStream(idToPath.get(i))) {
-                Image image = new Image(is);
-                if (image.isError()) {
-                    System.err.println("Image failed to load: " + image.getException());
-                }
-                idToImage.put(i, image);
-            } catch (IOException e) {
-                System.err.println("Path not found: " + idToPath.get(i) + " for image " + i);
-            }
-        }
-    }
-
-    public Image getImage(int id) {
-        return idToImage.get(id);
+        loadImages(ComponentRegistry.getInstance().getIdToImagePath());
     }
 }
