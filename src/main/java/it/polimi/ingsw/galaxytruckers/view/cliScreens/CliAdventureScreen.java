13305c6cfd03f36e75fcad1cb5b0a86d3f11b732
LorenzoCarafa <lorenzo.carafa@mail.polimi.it>
2025-06-23 19:43:15 +0200

    added card to every cli screen

--- src/main/java/it/polimi/ingsw/galaxytruckers/view/cliScreens/CliAdventureScreen.java
+++ src/main/java/it/polimi/ingsw/galaxytruckers/view/cliScreens/CliAdventureScreen.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.galaxytruckers.view.cliScreens;
 
 import it.polimi.ingsw.galaxytruckers.network.client.ControllerToServer;
+import it.polimi.ingsw.galaxytruckers.view.DescriptionUtils;
+import it.polimi.ingsw.galaxytruckers.view.cliElements.CliAdventureCard;
 import it.polimi.ingsw.galaxytruckers.view.model.ClientModel;
+import it.polimi.ingsw.galaxytruckers.view.model.adventureCards.AdventureCard;
 import it.polimi.ingsw.galaxytruckers.view.model.shipBuilding.ShipBoard;
 import it.polimi.ingsw.galaxytruckers.view.model.state.AdventureState;
 
@@ -13,12 +16,19 @@ public abstract class CliAdventureScreen extends CliScreen{
     protected final boolean isMyTurn;
     protected final boolean imOut;
     protected final ShipBoard currentShip;
+    protected final CliAdventureCard currentAdventureCard;
 
     public CliAdventureScreen(ClientModel model, ControllerToServer controller, AdventureState gameState) {
         super(model, controller, gameState);
         imOut = gameState.getImOut();   //if player surrendered
         currentShip = gameState.getShipBoard(); // ship of the current player playing
         isMyTurn = currentShip == myShipBoard;
+        AdventureCard adventureCard = model.getGame().getCurrentCard();
+        if(adventureCard != null) this.currentAdventureCard = new CliAdventureCard(adventureCard);
+        else this.currentAdventureCard = null;
+    }
 
+    protected List<String> printCurrentCard() {
+        return DescriptionUtils.borderAndTitle(currentAdventureCard.getDescription(), "Current card");
     }
 }
